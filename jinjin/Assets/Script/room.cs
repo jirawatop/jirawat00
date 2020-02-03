646cb987a6a12c2cf4826242f5e75f622db7eabe
jirawatop <jirawat.pi.60@gmail.com>
2020-02-04 02:42:28 +0700

    day3
    day 3 fuck you

---
+++ jinjin/Assets/Script/room.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class room : MonoBehaviour
+{
+    public GameObject virtualCam;
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player") && !collider.isTrigger)
+        {
+            virtualCam.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player")&&!collider.isTrigger)
+        {
+            virtualCam.SetActive(false);
+        }
+    }
+}
